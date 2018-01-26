65292c507622f67956ba011ee028193072d75df4
axetroy <troy450409405@gmail.com>
2018-01-26 16:41:40 +0800

    docs: Add an example for vue component

---
+++ examples/vue-component/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Hello world</h2>
+    <h2>This is an Vue example component</h2>
+  </div>
+</template>
+
+<script>
+console.log("Load component");
+export default {
+  name: "index"
+};
+</script>
+
+<style scoped>
+
+</style>
